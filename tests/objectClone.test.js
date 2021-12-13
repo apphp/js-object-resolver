5b0a1972a553d8bd67310d8f067c6f1b918c7a97
sam <samuela@hackeruso.com>
2021-12-13 17:03:55 +0200

    Added additional tests for objectGetNestedProperty method

--- tests/objectClone.test.js
+++ tests/objectClone.test.js
@@ -1,39 +1,39 @@
 const {objectClone: objectCloneTest} = require('../dist/object-resolver');
 
-describe('Test function objectClone', ()=>{
+describe('Test function objectClone', () => {
 
-  test('Given an undefined value', async()=>{
-      const obj = undefined;
-      const objCopy = objectCloneTest();
-      expect(objCopy).toBe(obj);
+  test('Given an undefined value', async () => {
+    const obj = undefined;
+    const objCopy = objectCloneTest();
+    expect(objCopy).toBe(obj);
   })
 
-  test('Given an empty value', async()=>{
+  test('Given an empty value', async () => {
     const obj = {};
     const objCopy = objectCloneTest(obj);
     expect(objCopy).toEqual(obj);
   })
 
-  test('Given a null value', async()=>{
+  test('Given a null value', async () => {
     const obj = null;
     const objCopy = objectCloneTest(obj);
     expect(objCopy).toBeNull();
   })
 
-  test('Given a simple object value', async()=>{
-    const obj = {'a':1};
+  test('Given a simple object value', async () => {
+    const obj = {'a': 1};
     const objCopy = objectCloneTest(obj);
     expect(objCopy).toEqual(obj);
   })
 
-  test('Given a nested object value', async()=>{
-    const obj = {'a':{'b':2}, 'c':3};
+  test('Given a nested object value', async () => {
+    const obj = {'a': {'b': 2}, 'c': 3};
     const objCopy = objectCloneTest(obj);
     expect(obj).toEqual(objCopy);
   })
 
-  test('Given a nested object value with null and undefined', async()=>{
-    const obj = {'a':{'b':2}, 'c':3, 'd':null, 'e':undefined};
+  test('Given a nested object value with null and undefined', async () => {
+    const obj = {'a': {'b': 2}, 'c': 3, 'd': null, 'e': undefined};
     const objCopy = objectCloneTest(obj);
     expect(objCopy).toEqual(obj);
   })
--- tests/objectGetNestedProperty.test.js
+++ tests/objectGetNestedProperty.test.js
@@ -1,9 +1,32 @@
-const {objectGetNestedProperty: objectGetNestedPropertyTest} = require('../dist/object-resolver');
+const {objectGetNestedProperty: objectGetNestedPropertyTest, objectCloneTest} = require('../dist/object-resolver');
 
-describe('Test function objectGetNestedProperty', ()=>{
+describe('Test function objectGetNestedProperty', () => {
 
-  test('test', async()=>{
-      expect(1).toBe(1);
+  test('Given an undefined values', async () => {
+    const prop = undefined;
+    const receivedProp = objectGetNestedPropertyTest();
+    expect(receivedProp).toBe(prop);
+  })
+
+  test('Receive a simple not existing scalar prop', async () => {
+    const obj = {'a':1};
+    const expectedProp = undefined;
+    const receivedProp = objectGetNestedPropertyTest(obj, 'b');
+    expect(receivedProp).toBe(expectedProp);
+  })
+
+  test('Receive a simple not existing scalar prop default value defined', async () => {
+    const obj = {'a':1};
+    const expectedProp = 2;
+    const receivedProp = objectGetNestedPropertyTest(obj, 'b', 2);
+    expect(receivedProp).toBe(expectedProp);
+  })
+
+  test('Receive a simple existing scalar prop', async () => {
+    const obj = {'a':1};
+    const expectedProp = 1;
+    const receivedProp = objectGetNestedPropertyTest(obj, 'a');
+    expect(receivedProp).toBe(expectedProp);
   })
 
 })
