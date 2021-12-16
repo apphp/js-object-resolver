a16be53cbcc0c7e9d3b2819ef4bf873e645e795e
sam <samuela@hackeruso.com>
2021-12-16 23:10:02 +0200

    Improved code of hasNestedProperty() function

--- dist/object-resolver.js
+++ dist/object-resolver.js
@@ -4,6 +4,82 @@
 
 "use strict";
 
+/**
+ |------------------------------------------------------------
+ | Main functions
+ |------------------------------------------------------------
+ */
+
+
+/**
+ * Check if object has nested property and returns it or default value
+ * @usage: console.log(getNestedProperty(obj, 'innerObject.deepObject.value', false));
+ */
+const getNestedProperty = function (objParam, propertyPath, defaultValue) {
+  let obj = objParam;
+
+  if (!obj || !propertyPath) {
+    return returnValue(obj, defaultValue);
+  }
+
+  const properties = propertyPath.split('.');
+
+  for (let i = 0; i < properties.length; i++) {
+    const prop = properties[i];
+    if (!(obj = validatePropertyExists(obj, prop))) {
+      break;
+    }
+
+    obj = obj[prop];
+  }
+
+  return returnValue(obj, defaultValue);
+}
+
+/**
+ * Check if object has nested property
+ * @usage: console.log(hasNestedProperty(obj, 'innerObject.deepObject.value'));
+ */
+const hasNestedProperty = function (objParam, propertyPath) {
+  let obj = objParam;
+  let isFound = true;
+
+  if (!obj || !propertyPath) {
+    return false;
+  }
+
+  const properties = propertyPath.split('.');
+
+  for (let i = 0; i < properties.length; i++) {
+    const prop = properties[i];
+    if (!(obj = validatePropertyExists(obj, prop))) {
+      isFound = false;
+      break;
+    }
+
+    obj = obj[prop];
+  }
+
+  return isFound;
+}
+
+/**
+ * Deep cloning - create a real clone of object (not by reference)
+ * Usable for cloning objects, that includes other objects
+ * @usage: objCopy = cloneObject(obj);
+ */
+const cloneObject = function (obj) {
+  if (!obj) {
+    return obj;
+  }
+  return JSON.parse(JSON.stringify(obj));
+}
+
+/**
+ |------------------------------------------------------------
+ | Helpers
+ |------------------------------------------------------------
+ */
 
 /**
  * Prepare return value
@@ -23,50 +99,16 @@ const returnValue = function (objParam, defaultValue) {
 }
 
 /**
- * Check if object has nested property and returns it or default value
- * @usage: console.log(getNestedProperty(obj, 'innerObject.deepObject.value', false));
+ * Validates if property exists
+ * @param obj
+ * @param prop
  */
-const getNestedProperty = function (objParam, propertyPath, defaultValue) {
-  let obj = objParam;
-
-  if (!obj || !propertyPath) {
-    return returnValue(obj, defaultValue);
+const validatePropertyExists = function (obj, prop) {
+  let result = obj
+  if (!obj || !obj.hasOwnProperty(prop)) {
+    result = undefined;
   }
-
-  const properties = propertyPath.split('.');
-
-  for (let i = 0; i < properties.length; i++) {
-    const prop = properties[i];
-
-    if (!obj || !obj.hasOwnProperty(prop)) {
-      obj = undefined;
-      break;
-    }
-
-    obj = obj[prop];
-  }
-
-  return returnValue(obj, defaultValue);
-}
-
-/**
- * Check if object has nested property
- * @usage: console.log(hasNestedProperty(obj, 'innerObject.deepObject.value'));
- */
-const hasNestedProperty = function (obj, propertyPath) {
-  return getNestedProperty(obj, propertyPath) !== undefined;
-}
-
-/**
- * Deep cloning - create a real clone of object (not by reference)
- * Usable for cloning objects, that includes other objects
- * @usage: objCopy = cloneObject(obj);
- */
-const cloneObject = function (obj) {
-  if (!obj) {
-    return obj;
-  }
-  return JSON.parse(JSON.stringify(obj));
+  return result;
 }
 
 
