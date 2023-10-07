afaf466b36280932107421edf0984ac87e9db777
samuelakopyan <leumas.a@gmail.com>
2023-10-07 15:19:47 +0300

    Added validation for cloneStructure in tests if nodejs < v17

--- tests/cloneStructure.test.js
+++ tests/cloneStructure.test.js
@@ -1,20 +1,41 @@
 const {cloneStructure: cloneStructureTest} = require('../dist/object-resolver');
 
+// Check the Node.js version
+const nodeMajorVersion = parseInt(process.versions.node.split('.')[0]);
+
 describe('Test function cloneStructure', () => {
 
   test('Given an undefined value', async () => {
+    if (nodeMajorVersion < 17) {
+      // Skip the test for Node.js versions below 17
+      console.log('Skipping test because Node.js version is less than 17');
+      return;
+    }
+
     const obj = undefined;
     const objCopy = cloneStructureTest(obj);
     expect(objCopy).toBe(obj);
   })
 
   test('Given a null value', async () => {
+    if (nodeMajorVersion < 17) {
+      // Skip the test for Node.js versions below 17
+      console.log('Skipping test because Node.js version is less than 17');
+      return;
+    }
+
     const obj = null;
     const objCopy = cloneStructureTest(obj);
     expect(objCopy).toBe(obj);
   })
 
   test('Given a simple object value', async () => {
+    if (nodeMajorVersion < 17) {
+      // Skip the test for Node.js versions below 17
+      console.log('Skipping test because Node.js version is less than 17');
+      return;
+    }
+
     const obj = {'a': 1};
     const objCopy = cloneStructureTest(obj);
     expect(objCopy).toBe(obj);
